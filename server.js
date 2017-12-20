1c645605708584aa0f746277b86d8c7e2e74cf1e
Luca Pilloni <pilloni.luca@hotmail.fr>
2017-12-20 13:04:22 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page',
+//     pageContent: 'Sorry, we will be back soon :)'
+//   });
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello! This is Home Page!'
+  });
+});
+
+app.get('/about',(req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    pageContent: 'a page about da app!'
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.status(404).send('<h1>Error: 404</h1>' + '<h2>Page Not Found</h2>')
+});
+
+app.listen(3000, () => {
+  console.log('App server is up on http://localhost:3000');
+});
+
